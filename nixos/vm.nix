9955fe1fcc5911d0c14ddbdc0e7b6a50cb046309
ryvrook <29802327+ryvrook@users.noreply.github.com>
2026-07-26 18:19:30 -0400

    use host qemu

--- nixos/vm.nix
+++ nixos/vm.nix
@@ -1,8 +1,16 @@
 {
   lib,
   modulesPath,
+  pkgs,
   ...
 }:
+let
+  qemu = pkgs.runCommand "host-qemu" { inherit (pkgs.qemu) meta; } ''
+    mkdir -p "$out/bin"
+    ln -s /usr/bin/qemu-img "$out/bin/qemu-img"
+    ln -s /usr/bin/qemu-system-x86_64 "$out/bin/qemu-system-x86_64"
+  '';
+in
 {
   imports = [
     "${modulesPath}/virtualisation/qemu-vm.nix"
@@ -31,6 +39,7 @@
     cores = 4;
     diskSize = 20480;
     graphics = true;
+    qemu.package = qemu;
     qemu.options = [
       "-device virtio-vga-gl"
       "-display gtk,gl=on"
