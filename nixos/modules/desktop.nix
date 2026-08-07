abbfd30d08ed809e60ec27359f64bba2a8520443
ryvrook <29802327+ryvrook@users.noreply.github.com>
2026-08-07 13:41:17 -0400

    directscanning freeze fix

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -3,4 +3,7 @@
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
   services.system76-scheduler.enable = true;
+
+  # Disable direct scanout to avoid freezing
+  environment.sessionVariables.COSMIC_DISABLE_DIRECT_SCANOUT = "true";
 }
